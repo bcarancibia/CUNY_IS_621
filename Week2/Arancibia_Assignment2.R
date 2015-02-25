df9bef09c604f54d5db172dc25d92d6450b33dbe
bcarancibia <bcarancibia@gmail.com>
2015-02-25 13:51:33 -0500

    start hw

--- Week2/Arancibia_Assignment2.R
+++ Week2/Arancibia_Assignment2.R
@@ -60,3 +60,48 @@ perceptron(x, y, w, a, verbose=T)
 
 
 #2 Design Multivariant Solution
+
+# a) It is reasonable to assume that the dataset is independent. A coin mass, thickness, and diameter are independent from coin to coin, 
+# meaning that the variables of one coin do not effect the variables of another coin. 
+
+
+# b)
+
+# Some variable definitions cent
+mu1 <- 2.5 # expected value of mass
+mu2 <- 19.05	# expected value of diameter
+mu3 <- 1.52 #thickness
+sig1 <- 0.0025	# variance of mass
+sig2 <- 0.1452 # variance of diameter
+sig3 <- 0.0009
+rho <- 0.5	# corr(x, y)
+
+# Some additional variables for x-axis and y-axis 
+xm <- -3
+xp <- 3
+ym <- -3
+yp <- 3
+
+x <- seq(xm, xp, length= as.integer((xp + abs(xm)) * 10))  # vector series x
+y <- seq(ym, yp, length= as.integer((yp + abs(ym)) * 10))  # vector series y
+
+# Core function
+bivariate <- function(x,y){
+  term1 <- 1 / (2 * pi * sig1 * sig2 * sqrt(1 - rho^2))
+  term2 <- (x - mu1)^2 / sig1^2
+  term3 <- -(2 * rho * (x - mu1)*(y - mu2))/(sig1 * sig2)
+  term4 <- (y - mu2)^2 / sig2^2
+  z <- term2 + term3 + term4
+  term5 <- term1 * exp((-z / (2 *(1 - rho^2))))
+  return (term5)
+}
+
+# Computes the density values
+z <- outer(x,y,bivariate)
+
+# Plot
+persp(x, y, z, main = "Bivariate Normal Distribution",
+      sub = bquote(bold(mu[1])==.(mu1)~", "~sigma[1]==.(sig1)~", "~mu[2]==.(mu2)~
+                     ", "~sigma[2]==.(sig2)~", "~rho==.(rho)),
+      col="orchid2", theta = 55, phi = 30, r = 40, d = 0.1, expand = 0.5,
+      ltheta = 90, lphi = 180, shade = 0.4, ticktype = "detailed", nticks=5)
