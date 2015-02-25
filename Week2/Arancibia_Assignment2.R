f1c8f144058b2cd968ad4e7d5f04a02440af99b2
bcarancibia <bcarancibia@gmail.com>
2015-02-24 22:15:44 -0500

    start hw

--- Week2/Arancibia_Assignment2.R
+++ Week2/Arancibia_Assignment2.R
@@ -6,122 +6,57 @@
 training.data.set <- read.csv("/users/bcarancibia/CUNY_IS_621/Week2/data/test-public.csv", header = TRUE)
 test.data.set <- read.csv("/users/bcarancibia/CUNY_IS_621/Week2/data/test-private.csv", header = TRUE)
 
-# input: x an array
-# output: y = 1/ (1 + exp(-x))
-sign <- function(x) {
-  return(1 / (1 + exp(-x)))
-}
+#0 = cent 1 = dime in training data set
 
-# threshold binary function 
-# input: x an array
-# output: y = ifelse(x > 0, 1, 0)
-thresh.bin <- function(x) {
-  return((x > 0) * 1.)
-}
+# Args:
+#   x: an m by n matrix representing the data under study
+#   y: objective vector of m elements
+#   w: initial guess for the weight vector, n elements
+#   a: learning rate, should be in (0,1]
+#   verbose: echo information w at each iteration (default: False)
 
-# function compute error
-# input: 
-#	model: list(weights, bias)
-#	x: a n*d matrix in which each row is a datum point
-#	y: a n*1 matrix in which each row is the target of x[i,]
-# output: classification error
-compute.error <- function(model, x, y) {
-  n.cases <- dim(x)[1]
-  err	<- sum((y - thresh.bin(x %*% model$weights + model$bias))^2)/n.cases
-  return(err)
-}
-
-compute.accuracy <- function(model, x, y) {
-  n.cases <- dim(x)[1]
-  err	<- sum(abs(y - thresh.bin(x %*% model$weights + model$bias)))/n.cases
-  return(1-err)
-}
-
-# online-learning perceptron
-# input:
-#	x: a n*d matrix in which each row is a datum point
-#	y: a n*1 matrix in which each row is the target of x[i,]
-#	maxit: the number of iterations for training
-#	learn.rate: the learning rate for training
-#	animation: TRUE for plotting examples and hyperplan (ONLY when d == 2)
-#	ani.step: the graph is shown each ani.step
-#	ani.delay: the delay time (sec) between two plots
-# output: list(weights, bias, errors)
-perceptron <- function(x, y, maxit = 100, learn.rate = 0.1,
-                       stepbystep = FALSE, 
-                       animation = FALSE, ani.step = 10, ani.delay = 0.5) {
-  in.dim	<- dim(x)[2]
-  n.cases	<- dim(x)[1]
-  weights	<- runif(in.dim, min=-0, max=0)
-  bias	<- runif(1, min=-0, max=0)
-  errors	<- rep(0, maxit)
-  act.function <- thresh.bin
-  
-  cat('------ init -----\n')
-  cat('weights ', weights, '\n');
-  cat('bias ', bias, '\n');
-  cat('learning rate: ', learn.rate, '\n\n');
-  
-  for (i in 1:maxit) {
-    # randomly pick an example and then predict its target
-    id		<- floor(runif(1,min=1,max=n.cases+1))
-    if (stepbystep == T) {
-      id <- (i-1) %% n.cases + 1
-      cat('pick x[',id,'] = ',x[id,],'\n')
-      readline()
-    }
-    
-    feat	<- x[id,]
-    targ	<- y[id,]
-    pred	<- act.function(sum(weights*feat)+bias)
-    
-    # update weights and bias
-    weights <- weights + learn.rate * (targ-pred) * feat
-    bias	<- bias + learn.rate * (targ-pred)
-    
-    if (stepbystep == T) {
-      cat('target ', targ, '\n');
-      cat('prediction ', pred, '\n');
-      cat('weights ', weights, '\n');
-      cat('bias ', bias, '\n\n\n');
-    }
-    
-    # compute error
-    errors[i] <- compute.error(list(weights = weights, bias = bias), x, y)
-    
-    # plot
-    if (in.dim == 2 && animation == TRUE && i %% ani.step == 0) {
-      d1.min <- min(x[,1]) - 2; d1.max <- max(x[,1]) + 2
-      d2.min <- min(x[,2]) - 2; d2.max <- max(x[,2]) + 2
-      
-      plot(x[y==0,1], x[y==0,2], col='blue', 
-           xlim = c(d1.min,d1.max), ylim = c(d2.min,d2.max),
-           xlab = colnames(x)[1], ylab = colnames(x)[2])
-      points(x[y==1,1], x[y==1,2], col='red')
-      points(feat[1], feat[2], col='black')
-      
-      d1 <- seq(d1.min, d1.max, by=0.1)
-      d2 <- (-bias - d1*weights[1]) / weights[2];
-      lines(d1, d2)
-      Sys.sleep(ani.delay)
-    }
+perceptron <- function(x, y, w, a, verbose=F) {
+  hyp <- function(w, x) {
+    p <- w %*% x
+    sign(p - abs(p)) + 1
   }
   
-  return(list(weights = weights, bias = bias, errors = errors))
+  iter <- function(x, y, w, a) {
+    w + a * (y - hyp(w, x)) * x
+  }
+  
+  wold <- matrix(w, nrow=1)
+  nrows <- dim(x)[1]
+  done <- FALSE
+  iteration <- 0
+  
+  while (! done) {
+    if (verbose) {
+      print(paste("Iteration", iteration, sep=" "), quote=F)
+      print(wold)
+    }
+    iteration <- iteration + 1
+    count <- 0
+    for (i in 1:nrows) {
+      wnew = iter(x[i,], y[i], wold, a)
+      if (all(wnew == wold)) {
+        count <- count + 1
+      }
+      wold <- wnew
+    }
+    if (count == nrows) {
+      done <- TRUE
+    }
+  }
+  wnew
 }
 
-# split data
-# input: 
-#	x: a n*d matrix in which each row is a datum point
-#	y: a n*1 matrix in which each row is the target of x[i,]
-#	ratio: n_train / n_test
-# output: list(x.train, y.train, x.test, y.test)
-split.data <- function(x, y, ratio) {
-  ncases <- dim(x)[1]
-  ntrain <- round(ncases * ratio / (ratio+1))
-  list(
-    x.train = x[1:ntrain,],
-    y.train = as.matrix(y[1:ntrain,]),
-    x.test = x[(ntrain+1):ncases,],
-    y.test = as.matrix(y[(ntrain+1):ncases,]))
-}
+# Example
+x <- matrix(c(1, 2, 1, 2, 2, 1, 4, 1, 1, 5, 1, 1), nrow=4, byrow=T)
+y <- c(1, 0, 1, 0)
+w <- c(0, 2, 1)
+a <- 0.5
+perceptron(x, y, w, a, verbose=T)
+
+
+#2 Design Multivariant Solution
