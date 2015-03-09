bacbd360cc1f10425eac04b3c4dcaf6c63f83c6a
bcarancibia <bcarancibia@gmail.com>
2015-03-08 22:32:37 -0400

    update

--- Week3/Arancibia_Assignment3.R
+++ Week3/Arancibia_Assignment3.R
@@ -3,7 +3,11 @@
 
 #Week 3
 
-df <- read.csv("/users/bcarancibia/CUNY_IS_621/Week3/data/sample-training-data-naive-bayes.csv")
+#1
+
+df1 <- read.csv("/users/bcarancibia/CUNY_IS_621/Week3/data/sample-training-data-naive-bayes.csv")
+df2 <- data.table(read.table('/users/bcarancibia/CUNY_IS_621/Week3/data/sample-testing-data-naive-bayes.csv', header=TRUE))
+
 df$Cat1 <- ifelse(df$Cat1.numeric=="A",1,-1)
 inputs <- as.matrix(df)
 #1)
@@ -34,6 +38,7 @@ classify <- function (classifier, sample) {
 }
 
 training_set <- data.table(read.table('/users/bcarancibia/CUNY_IS_621/Week3/data/sample-training-data-naive-bayes.csv', header=TRUE))
+test_set <- data.table(read.table('/users/bcarancibia/CUNY_IS_621/Week3/data/sample-testing-data-naive-bayes.csv', header=TRUE))
 
 sample = data.table(Cat1='B', Cat2='H', Cat3='Z')
 
@@ -58,3 +63,98 @@ test
   
 
 #2) 
+
+df3 <- read.csv('/users/bcarancibia/CUNY_IS_621/Week3/data/sample-testing-data-nearest-neighbor.csv', header=TRUE)
+df4 <- read.csv('/users/bcarancibia/CUNY_IS_621/Week3/data/sample-training-data-nearest-neighbor.csv', header=TRUE)
+
+k.nearest.neighbors <- function(i, distance.matrix, k = 5)
+{
+  ordered.neighbors <- order(distance.matrix[i, ])   # closest to row i in descending order.
+  return(ordered.neighbors[2:(k + 1)])
+}
+
+k.nearest.neighbors(10, d, 10)
+knn <- function(df, k = 5)
+{
+  distance <- as.matrix(dist(df[,1:2]))
+  predictions <- rep(0, nrow(df))
+  
+  # For every point in the dataset
+  for (i in 1:nrow(df))
+  {
+    indices <- k.nearest.neighbors(i, distance, k = k)  # Get the nearest neighbors
+    predictions[i] <- ifelse(mean(df[indices, 'Class']) > 0.5, 1, -1)   # If > 0.5, then this is in class "1"
+  }
+  return(predictions)
+}
+
+predictions <- knn(df4, 5)
+df4$predictions <- predictions
+sum(df4$Class != df4$predictions)
+
+#Testing
+
+predictions <- knn(df2, 5)
+df2$predictions <- predictions
+sum(df2$Class != df2$predictions)
+
+
+#3 
+
+#####
+#See Azure ML learning website
+####
+
+
+#4
+
+require(e1071)
+
+classifier <- naiveBayes(Class ~ Cat1 + Cat2 + Cat3, data = df1)
+test <- unique(df)
+test$Prediction <- predict(classifier, test)
+test
+
+classifier <- naiveBayes(Class ~ Cat1 + Cat2 + Cat3, data = df2)
+test <- unique(df2)
+test$Prediction <- predict(classifier, test)
+test
+
+#####
+The results are not identical to my results. 
+####
+
+require(class)
+
+knn_predict <- knn(df3, k=5)
+
+####
+#The class package, as any nearest neighbors algorithm, depends on the k value picked. I think the knn class works faster than my implemented algorithm.
+#The results were not the exact same
+####
+
+#5
+
+require(e1071)
+
+df5 <- read.csv('/users/bcarancibia/CUNY_IS_621/Week3/data/jury-training-data.csv', header=TRUE)
+df6 <- read.csv('/users/bcarancibia/CUNY_IS_621/Week3/data/jury-learning-data-public.csv', header=TRUE)
+df7 <- read.csv('/users/bcarancibia/CUNY_IS_621/Week3/data/jury-learning-data-private.csv', header=TRUE)
+
+classifier <- naiveBayes(tendency ~ agegroup + employment + gender + marital, data = df5)
+test <- unique(df7)
+test$Prediction <- predict(classifier, test)
+table(test)
+
+#based on the classifier, the following is predicted on the private dataset:
+
+summary(test)
+
+#It seems that , , gender = Female, marital = Married, Prediction = Guilty has the largest influencer on the prediction values.
+
+#6
+
+
+
+
+
